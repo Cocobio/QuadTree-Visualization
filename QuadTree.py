b52a97a8172df51122ae0c8ce08a0f5876d815a3
cocobio <iosorio@udec.cl>
2023-09-04 23:12:53 -0300

    Small patch for working when the dataset is not in disk.

--- QuadTree.py
+++ QuadTree.py
@@ -11,253 +11,264 @@ NE = 1
 SW = 2
 SE = 3
 
-class Node:
-	def __init__(self, t=GREY, x=0.0, y=0.0, i=0):
-		self.children = [None, None, None, None]
-		self.x = x
-		self.y = y
-		self.info = i
-		self.type = t
 
-		self.selected = False
+class Node:
+    def __init__(self, t=GREY, x=0.0, y=0.0, i=0):
+        self.children = [None, None, None, None]
+        self.x = x
+        self.y = y
+        self.info = i
+        self.type = t
+
+        self.selected = False
+
 
 def compare(p, x, y):
-	if p.x<x:
-		if p.y>y:
-			return SW
-		else:
-			return NW
+    if p.x < x:
+        if p.y > y:
+            return SW
+        else:
+            return NW
+    elif p.y > y:
+        return SE
+    else:
+        return NE
 
-	elif p.y>y:
-		return SE
-	else:
-		return NE
 
 class QuadTree:
+    def __init__(self, width, height):
+        self.XF = [-0.25, 0.25, -0.25, 0.25]
+        self.YF = [-0.25, -0.25, 0.25, 0.25]
 
-	def __init__(self, width, height):
-		self.XF = [-0.25,0.25,-0.25,0.25]
-		self.YF = [-0.25,-0.25,0.25,0.25]
+        self.width = float(width)
+        self.height = float(height)
 
-		self.width = float(width)
-		self.height = float(height)
+        self.box = pygame.Rect(0, 0, width, height)
 
-		self.box = pygame.Rect(0, 0, width, height)
+        self.quadrant_color_green = (92, 210, 95)
+        self.quadrant_color_red = (236, 42, 127)
+        self.quadrant_color = self.quadrant_color_red
 
-		self.quadrant_color_green = (92,210,95)
-		self.quadrant_color_red = (236,42,127)
-		self.quadrant_color = self.quadrant_color_red
+        self.point_color = (236, 168, 42)
+        self.point_color_accent = (235, 0, 54)
 
-		self.point_color = (236,168,42)
-		self.point_color_accent = (235,0,54)
+        self.root = None
 
-		self.root = None
+        self.size = 0
 
-		self.size = 0
+        # Drawing
+        self.bb = False
+        # for drawing
+        self.searching = False
 
-		## Drawing
-		self.bb = False
-		# for drawing
-		self.searching = False
+    def add_point(self, x, y, i=0):
+        lx = self.width
+        ly = self.height
+        center = [self.width/2.0, self.height/2.0]
 
-	def add_point(self, x, y, i=0):
-		lx = self.width
-		ly = self.height
-		center = [self.width/2.0, self.height/2.0]
+        p = Node(BLACK, x, y, i)
 
-		p = Node(BLACK,x,y,i)
+        if not self.root:
+            self.root = p
+            self.size += 1
+            return
 
-		if self.root == None:
-			self.root = p
-			self.size += 1
-			return
-		
+        if self.root.type == BLACK:
+            if self.root.x == x and self.root.y == y:
+                return
+            u = self.root
+            self.root = Node(GREY)
 
-		if self.root.type == BLACK:
-			if self.root.x == x and self.root.y == y:
-				return
-			u = self.root
-			self.root = Node(GREY)
+            q = compare(u, *center)
+            self.root.children[q] = u
 
-			q = compare(u, *center)
-			self.root.children[q] = u
+        t = self.root
+        q = compare(p, *center)
 
-		t = self.root
-		q = compare(p,*center)
+        while t.children[q] and t.children[q].type == GREY:
+            t = t.children[q]
+            center[0] += self.XF[q]*lx
+            center[1] += self.YF[q]*ly
 
-		while t.children[q] != None and t.children[q].type == GREY:
-			t = t.children[q]
-			center[0] += self.XF[q]*lx
-			center[1] += self.YF[q]*ly
+            lx /= 2
+            ly /= 2
 
-			lx /= 2
-			ly /= 2
+            q = compare(p, *center)
 
-			q = compare(p,*center)
+        if not t.children[q]:
+            t.children[q] = p
+        elif t.children[q].x == x and t.children[q].y == y:
+            return
+        else:
+            u = t.children[q]
+            uq = q
 
-		if t.children[q] == None:
-			t.children[q] = p
-		elif t.children[q].x == x and t.children[q].y == y:
-			return
-		else:
-			u = t.children[q]
-			uq = q
+            while q == uq:
+                t.children[q] = Node(GREY)
+                t = t.children[q]
 
-			while q==uq:
-				t.children[q] = Node(GREY)
-				t = t.children[q]
+                center[0] += self.XF[q]*lx
+                center[1] += self.YF[q]*ly
 
-				center[0] += self.XF[q]*lx
-				center[1] += self.YF[q]*ly
+                lx /= 2
+                ly /= 2
 
-				lx /= 2
-				ly /= 2
+                q = compare(p, *center)
+                uq = compare(u, *center)
 
-				q = compare(p,*center)
-				uq = compare(u,*center)
+            t.children[q] = p
+            t.children[uq] = u
 
-			t.children[q] = p
-			t.children[uq] = u
+        self.size += 1
 
-		self.size += 1
+    def remove_point(self, x, y):
+        lx = self.width
+        ly = self.height
+        center = [self.width/2.0, self.height/2.0]
 
-	def remove_point(self, x, y):
-		lx = self.width
-		ly = self.height
-		center = [self.width/2.0, self.height/2.0]
+        p = Node(BLACK, x, y)
 
-		p = Node(BLACK,x,y)
+        if not self.root:
+            return
+        elif self.root.type == BLACK:
+            if self.root.x == p.x and self.root.y == p.y:
+                self.root = None
+                self.size -= 1
+            return
 
-		if self.root == None:
-			return
-		elif self.root.type == BLACK:
-			if self.root.x == p.x and self.root.y == p.y:
-				self.root = None
-				self.size -= 1
-			return
+        t = self.root
+        f = None
 
-		t = self.root
-		f = None
+        while True:
+            q = compare(p, *center)
 
-		while True:
-			q = compare(p,*center)
+            if not t.children[q]:
+                return
 
-			if t.children[q] == None:
-				return
+            if t.children[q].type == GREY and (t.children[q ^ 1] or
+                                               t.children[q ^ 2] or
+                                               t.children[q ^ 3]):
+                f = t
+                qf = q
 
-			if t.children[q].type == GREY and (t.children[q^1]!=None or t.children[q^2]!=None or t.children[q^3]!=None):
-				f = t
-				qf = q
+            ft = t
+            t = t.children[q]
 
-			ft = t
-			t = t.children[q]
+            center[0] += self.XF[q]*lx
+            center[1] += self.YF[q]*ly
 
-			center[0] += self.XF[q]*lx
-			center[1] += self.YF[q]*ly
+            lx /= 2
+            ly /= 2
 
-			lx /= 2
-			ly /= 2
+            if not t or t.type != GREY:
+                break
 
-			if t==None or t.type!=GREY:
-				break
+        if not t or t.x != p.x or t.y != p.y:
+            return
 
-		if t==None or t.x!=p.x or t.y!=p.y:
-			return
+        self.size -= 1
 
-		self.size -= 1
+        ft.children[q] = None
+        siblings = 0
+        for i in range(4):
+            if ft.children[i]:
+                if ft.children[i].type == GREY:
+                    return
+                siblings += 1
 
-		ft.children[q] = None
-		siblings = 0
-		for i in range(4):
-			if ft.children[i]!=None:
-				if ft.children[i].type==GREY:
-					return
-				siblings += 1
+        if siblings > 1:
+            return
 
-		if siblings>1:
-			return
+        t = self.root if not f else f.children[qf]
 
-		t = self.root if f==None else f.children[qf]
+        while t and t.type == GREY:
+            q = 0
+            while not t.children[q]:
+                q += 1
 
-		while t!=None and t.type==GREY:
-			q=0
-			while t.children[q]==None:
-				q += 1
+            tmp = t.children[q]
+            t.children[q] = None
+            t = tmp
 
-			tmp = t.children[q]
-			t.children[q] = None
-			t = tmp
+        if not f:
+            self.root = t
+        else:
+            f.children[qf] = t
 
-		if f==None:
-			self.root = t
-		else:
-			f.children[qf] = t
+    def recursive_draw(self, screen, node, x, y, w, h):
+        if node.type == BLACK:
+            if self.searching:
+                pygame.draw.circle(screen,
+                                   self.point_color_accent if node.selected
+                                   else self.point_color,
+                                   (int(node.x), int(node.y)), 2)
+            else:
+                pygame.draw.circle(screen, self.point_color,
+                                   (int(node.x), int(node.y)), 2)
+        elif node.type == GREY:
+            if self.bb:
+                pygame.draw.line(screen, self.quadrant_color, (x, y-h/2),
+                                 (x, y+h/2), 1)
+                pygame.draw.line(screen, self.quadrant_color, (x-w/2, y),
+                                 (x+w/2, y), 1)
 
+            for i in range(4):
+                if node.children[i]:
+                    self.recursive_draw(screen, node.children[i],
+                                        x+self.XF[i]*w, y+self.YF[i]*h,
+                                        w/2, h/2)
 
-	def recursive_draw(self, screen, node, x, y, w, h):
-		if node.type == BLACK:
-			if self.searching:
-				pygame.draw.circle(screen, self.point_color_accent if node.selected else self.point_color, (int(node.x), int(node.y)), 2)
-			else:
-				pygame.draw.circle(screen, self.point_color, (int(node.x), int(node.y)), 2)
-		elif node.type == GREY:
-			if self.bb:
-				pygame.draw.line(screen, self.quadrant_color, (x,y-h/2), (x,y+h/2), 1)
-				pygame.draw.line(screen, self.quadrant_color, (x-w/2,y), (x+w/2,y), 1)
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.quadrant_color, self.box, 2)
 
-			for i in range(4):
-				if node.children[i] != None:
-					self.recursive_draw(screen,node.children[i],x+self.XF[i]*w,y+self.YF[i]*h,w/2,h/2)
+        if self.root:
+            self.recursive_draw(screen, self.root, self.width/2, self.height/2,
+                                self.width, self.height)
 
-	def draw(self, screen):
-		pygame.draw.rect(screen, self.quadrant_color, self.box, 2)
-		lx = self.width
-		ly = self.height
+    def toggle_boundingbox(self):
+        self.bb = not self.bb
 
-		if self.root != None:
-			self.recursive_draw(screen, self.root, self.width/2, self.height/2, self.width, self.height)
+    def manual_insert(self, flag):
+        if flag:
+            self.quadrant_color = self.quadrant_color_green
+        else:
+            self.quadrant_color = self.quadrant_color_red
 
+    def set_searching(self, s):
+        self.searching = s
 
-	def toggle_boundingbox(self):
-		self.bb = not self.bb
+    def recursive_region_search(self, node, x, y, w, h,
+                                r_c, r_w, r_h, f_lambda):
+        if node.type == BLACK:
+            if fabs(node.x-r_c[0])*2 <= r_w and fabs(node.y-r_c[1])*2 <= r_h:
+                node.selected = True
+                f_lambda(node)
+            return
+        for i in range(4):
+            if node.children[i] and fabs(x+self.XF[i]*w-r_c[0])*2 <= w+r_w \
+                                and fabs(y+self.YF[i]*h-r_c[1])*2 <= h+r_h:
+                self.recursive_region_search(node.children[i],
+                                             x+self.XF[i]*w, y+self.YF[i]*h,
+                                             w/2, h/2, r_c, r_w, r_h, f_lambda)
 
-	def manual_insert(self, flag):
-		if flag:
-			self.quadrant_color = self.quadrant_color_green
-		else:
-			self.quadrant_color = self.quadrant_color_red
+    def search_region(self, center, w, h, f_lambda):
+        if not self.root:
+            return
 
-	def set_searching(self, s):
-		self.searching = s
+        # Clean selected ones
+        q = queue.Queue()
 
-	def recursive_region_search(self, node, x, y, w, h, r_c, r_w, r_h, f_lambda):
-		if node.type == BLACK:
-			if fabs(node.x-r_c[0])*2 <= r_w and fabs(node.y-r_c[1])*2 <= r_h:
-				node.selected = True
-				f_lambda(node)
-			return
-		for i in range(4):
-			if node.children[i] != None and fabs(x+self.XF[i]*w-r_c[0])*2 <= w+r_w and fabs(y+self.YF[i]*h-r_c[1])*2 <= h+r_h:
-				self.recursive_region_search(node.children[i], x+self.XF[i]*w, y+self.YF[i]*h, w/2, h/2, r_c, r_w, r_h, f_lambda)
+        q.put(self.root)
 
-	def search_region(self, center, w, h, f_lambda):
-		if self.root == None:
-			return
+        while not q.empty():
+            n = q.get()
+            if n.type == GREY:
+                for i in range(4):
+                    if n.children[i]:
+                        q.put(n.children[i])
+            else:
+                n.selected = False
 
-		# Clean selected ones
-		q = queue.Queue()
-
-		q.put(self.root)
-
-		while not q.empty():
-			n = q.get()
-			if n.type == GREY:
-				for i in range(4):
-					if n.children[i] != None:
-						q.put(n.children[i])
-			else:
-				n.selected = False
-		######################
-
-		self.recursive_region_search(self.root,self.width/2,self.height/2,self.width,self.height,center,w,h, f_lambda)
-
+        self.recursive_region_search(self.root, self.width/2, self.height/2,
+                                     self.width, self.height,
+                                     center, w, h, f_lambda)
--- QuadTreeVisualizer.py
+++ QuadTreeVisualizer.py
@@ -1,37 +1,36 @@
-##########################################################################################################################
-#
-# Key bindings:
-#	- B: Toggle the bounding box.
-#	- L: Lock the manual insertion. Denoted with bounding box on red.
-#
-#	- S: Toggles the search function and drawing the rect.
-#	- ARROW UP: Grow searching rect, verticaly.
-#	- ARROW DOWN: Shrink searching rect, verticaly.
-#	- ARROW LEFT: Shrink searching rect, horizontaly.
-#	- ARROW RIGHT: Grow searching rect, horizontaly.
-#
-#
-#	##### Must have the user_insert flag set as True #####
-#	- 1: Insert 1 city from data set.
-#	- 2: Insert 10 city from data set.
-#	- 3: Insert 100 city from data set.
-#	- 4: Insert 1000 city from data set.
-#	- 5: Insert 10000 city from data set.
-#
-##########################################################################################################################
+'''
+Key bindings:
+    - B: Toggle the bounding box.
+    - L: Lock the manual insertion. Denoted with bounding box on red.
 
+    - S: Toggles the search function and drawing the rect.
+    - ARROW UP: Grow searching rect, verticaly.
+    - ARROW DOWN: Shrink searching rect, verticaly.
+    - ARROW LEFT: Shrink searching rect, horizontaly.
+    - ARROW RIGHT: Grow searching rect, horizontaly.
+
+
+##### Must have the user_insert flag set as True #####
+    - 1: Insert 1 city from data set.
+    - 2: Insert 10 city from data set.
+    - 3: Insert 100 city from data set.
+    - 4: Insert 1000 city from data set.
+    - 5: Insert 10000 city from data set.
+
+'''
 
 import pygame
-import numpy as np
 from QuadTree import QuadTree
 
-def read_and_parse_from(f):
-	line = f.readline().split(";")
-	data = [float(i) for i in line[-1][:-2].split(',')]
-	data.append(int(line[4]))
-	return data
 
-background = (34,34,34)
+def read_and_parse_from(f):
+    line = f.readline().split(";")
+    data = [float(i) for i in line[-1][:-2].split(',')]
+    data.append(int(line[4]))
+    return data
+
+
+background = (34, 34, 34)
 
 # Screen size
 scale_factor = 5
@@ -44,13 +43,13 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Quadtree")
 
-q = QuadTree(width,height)
+q = QuadTree(width, height)
 
 search_rect = pygame.Rect(0, 0, 80, 80)
-search_color = (81,243,157)
+search_color = (81, 243, 157)
 
-text_color = (0,255,0)
-text_background = (0,0,255)
+text_color = (0, 255, 0)
+text_background = (0, 0, 255)
 
 modes = ['n of points: ', 'total population: ', 'REMOVING!']
 mode = 0
@@ -65,124 +64,135 @@ textRect.top = height - textRect.height - 2
 filename = "C:/worldcitiespop_fixed.csv"
 city_n = 20000
 
-## FLAGS
+# FLAGS
 running = True
 user_insert = True
 manual_insert = False
 searching = False
 
 if not user_insert:
-	with open(filename, 'r', encoding="utf8") as f:
-		f.readline()
+    with open(filename, 'r', encoding="utf8") as f:
+        f.readline()
 
-		for i in range(city_n):
-			data = read_and_parse_from(f)
-			q.add_point((data[1]+180)*scale_factor, (data[0]-90)*(-scale_factor),data[2])
+        for i in range(city_n):
+            data = read_and_parse_from(f)
+            q.add_point((data[1]+180)*scale_factor,
+                        (data[0]-90)*(-scale_factor), data[2])
 else:
-	f = open(filename, 'r', encoding="utf8")
-	f.readline()
-#############################################################
+    try:
+        f = open(filename, 'r', encoding="utf8")
+        f.readline()
+    except Exception:
+        pass
 
+# --------------------------------------------------------------------------- #
 while running:
-	# timer
-	# pygame.time.wait(2)
-	
-	# event attender
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+    # timer
+    # pygame.time.wait(2)
 
-			if manual_insert:
-				p = [i for i in pygame.mouse.get_pos()]
-				q.add_point(*p)
+    # event attender
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_b:
-				q.toggle_boundingbox()
-			elif event.key == pygame.K_l:
-				manual_insert = not manual_insert
-				q.manual_insert(manual_insert)
+            if manual_insert:
+                p = [i for i in pygame.mouse.get_pos()]
+                q.add_point(*p)
 
-			elif event.key == pygame.K_1:
-				if user_insert:
-					data = read_and_parse_from(f)
-					q.add_point((data[1]+180)*scale_factor, (data[0]-90)*(-scale_factor),data[2])
-			elif event.key == pygame.K_2:
-				if user_insert:
-					for j in range(10):
-						data = read_and_parse_from(f)
-						q.add_point((data[1]+180)*scale_factor, (data[0]-90)*(-scale_factor),data[2])
-			elif event.key == pygame.K_3:
-				if user_insert:
-					for j in range(100):
-						data = read_and_parse_from(f)
-						q.add_point((data[1]+180)*scale_factor, (data[0]-90)*(-scale_factor),data[2])
-			elif event.key == pygame.K_4:
-				if user_insert:
-					for j in range(1000):
-						data = read_and_parse_from(f)
-						q.add_point((data[1]+180)*scale_factor, (data[0]-90)*(-scale_factor),data[2])
-			elif event.key == pygame.K_5:
-				if user_insert:
-					for j in range(10000):
-						data = read_and_parse_from(f)
-						q.add_point((data[1]+180)*scale_factor, (data[0]-90)*(-scale_factor),data[2])
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_b:
+                q.toggle_boundingbox()
+            elif event.key == pygame.K_l:
+                manual_insert = not manual_insert
+                q.manual_insert(manual_insert)
 
-			elif event.key == pygame.K_n:
-				mode = (mode+1)%len(modes)
-			
+            elif event.key == pygame.K_1:
+                if user_insert:
+                    data = read_and_parse_from(f)
+                    q.add_point((data[1]+180)*scale_factor,
+                                (data[0]-90)*(-scale_factor), data[2])
+            elif event.key == pygame.K_2:
+                if user_insert:
+                    for j in range(10):
+                        data = read_and_parse_from(f)
+                        q.add_point((data[1]+180)*scale_factor,
+                                    (data[0]-90)*(-scale_factor), data[2])
+            elif event.key == pygame.K_3:
+                if user_insert:
+                    for j in range(100):
+                        data = read_and_parse_from(f)
+                        q.add_point((data[1]+180)*scale_factor,
+                                    (data[0]-90)*(-scale_factor), data[2])
+            elif event.key == pygame.K_4:
+                if user_insert:
+                    for j in range(1000):
+                        data = read_and_parse_from(f)
+                        q.add_point((data[1]+180)*scale_factor,
+                                    (data[0]-90)*(-scale_factor), data[2])
+            elif event.key == pygame.K_5:
+                if user_insert:
+                    for j in range(10000):
+                        data = read_and_parse_from(f)
+                        q.add_point((data[1]+180)*scale_factor,
+                                    (data[0]-90)*(-scale_factor), data[2])
 
-			## Modify the searching rect
-			elif event.key == pygame.K_s:
-				searching = not searching
-				q.set_searching(searching)
-			
+            elif event.key == pygame.K_n:
+                mode = (mode+1) % len(modes)
 
-	keys = pygame.key.get_pressed()  #checking pressed keys
+            # Modify the searching rect
+            elif event.key == pygame.K_s:
+                searching = not searching
+                q.set_searching(searching)
 
-	if keys[pygame.K_UP]:
-		search_rect.inflate_ip(0,1)
-	if keys[pygame.K_DOWN]:
-		search_rect.inflate_ip(0,-1)
-	if keys[pygame.K_LEFT]:
-		search_rect.inflate_ip(-1,0)
-	if keys[pygame.K_RIGHT]:
-		search_rect.inflate_ip(1,0)
+    keys = pygame.key.get_pressed()  # Checking pressed keys
 
+    if keys[pygame.K_UP]:
+        search_rect.inflate_ip(0, 1)
+    if keys[pygame.K_DOWN]:
+        search_rect.inflate_ip(0, -1)
+    if keys[pygame.K_LEFT]:
+        search_rect.inflate_ip(-1, 0)
+    if keys[pygame.K_RIGHT]:
+        search_rect.inflate_ip(1, 0)
 
-	# OpenGL Stuff
-	screen.fill(background)
+    # OpenGL Stuff
+    screen.fill(background)
 
-	# Draw rect for searching
-	l = []
-	if searching:
-		m_pos = pygame.mouse.get_pos()
-		search_rect.move_ip(m_pos[0]-search_rect.left-search_rect.width/2, m_pos[1]-search_rect.top-search_rect.height/2)
-		q.search_region((search_rect.left+search_rect.width/2, search_rect.top+search_rect.height/2), search_rect.width, search_rect.height, (lambda x, listing=l: listing.append(x)))
-		if(mode == len(modes)-1):
-			for node in l:
-				q.remove_point(node.x, node.y)
+    # Draw rect for searching
+    point_list = []
+    if searching:
+        m_pos = pygame.mouse.get_pos()
+        search_rect.move_ip(m_pos[0]-search_rect.left-search_rect.width/2,
+                            m_pos[1]-search_rect.top-search_rect.height/2)
+        q.search_region((search_rect.left+search_rect.width/2,
+                         search_rect.top+search_rect.height/2),
+                        search_rect.width, search_rect.height,
+                        (lambda x, listing=point_list: listing.append(x)))
 
-	# Draw quadtree
-	q.draw(screen)
+        if (mode == len(modes)-1):
+            for node in point_list:
+                q.remove_point(node.x, node.y)
 
-	if searching:
-		pygame.draw.rect(screen, search_color, search_rect, 2)
+    # Draw quadtree
+    q.draw(screen)
 
+    if searching:
+        pygame.draw.rect(screen, search_color, search_rect, 2)
 
-	## Text
-	if mode == 0:
-		text = font.render(modes[mode]+str(len(l)), True, text_color, text_background) 
-	elif mode == 1:
-		population = 0
-		for node in l:
-			population += node.info
-		text = font.render(modes[mode]+str(population), True, text_color, text_background)
-	else:
-		text = font.render(modes[mode], True, text_color, text_background)
+    # Text
+    if mode == 0:
+        text = font.render(modes[mode]+str(len(point_list)), True,
+                           text_color, text_background)
+    elif mode == 1:
+        population = 0
+        for node in point_list:
+            population += node.info
+        text = font.render(modes[mode]+str(population), True,
+                           text_color, text_background)
+    else:
+        text = font.render(modes[mode], True, text_color, text_background)
 
-	screen.blit(text, textRect)
+    screen.blit(text, textRect)
 
-	pygame.display.update()
-
+    pygame.display.update()
